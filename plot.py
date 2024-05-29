c59b364087ac1ae3f138655e7adec0aa241e60d0
YouthYou <148071547+YouthYouW@users.noreply.github.com>
2024-05-29 21:11:29 +0800

    Add files via upload

---
+++ plot.py
@@ -0,0 +1,436 @@
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+from matplotlib.patches import Circle
+from matplotlib.colors import Normalize
+from mpl_toolkits.mplot3d import art3d
+import numpy as np
+import cmath as cm
+from numpy import sin,cos,exp,sinh,cosh,pi,arctan,sqrt
+from scipy.special import jv, jvp, hankel1, h1vp, hankel2, h2vp,lpmv,spherical_jn,spherical_yn
+#直角坐标系
+k0=50
+dc = 0.3
+def cartesian(ratio,a,x,V0):
+    xmin, xmax = 0, a
+    x1 = np.linspace(-dc-xmin,xmin,1000)
+    x2 = np.linspace(xmin,xmax,1000)
+    x3 = np.linspace(xmax,xmax+dc,1000)
+    j=1j
+    k1, k2 = k0*np.sqrt(ratio)*np.sqrt(V0), k0*cm.sqrt(1-1/ratio)
+    #T = abs(k1-k2)*(k1+k2)*sin(a*k2)/(2*j*k1*k2*cos(a*k2)+(k1**2+k2**2)*sin(a*k2))
+    #R = abs(2*j*exp(-j*a*k1)*k1*k2/(2*j*k1*k2*cos(a*k2)+(k1**2+k2**2)*sin(a*k2)))
+    A1 = 1
+    #A2, C1 = 2*j*(k1**2-k2**2)*sin(k2*a)/((k1-k2)**2*exp(j*k2*a)-(k1+k2)**2*exp(-j*k2*a)), -4*k1*k2*exp(-j*k1*a)/((k1-k2)**2*exp(j*k2*a)-(k1+k2)**2*exp(-j*k2*a))
+    #B1, B2 = (k1+k2-A2*k1+A2*k2)/(2*k2), (k2+k1+A2*k1+A2*k2)/(2*k2)
+    
+    #D = -k1**2+exp(j*2*a*k2)*k1**2-2*k1*k2-2*exp(2*j*a*k2)*k1*k2-k2**2+exp(2*j*a*k2)*k2**2
+    #A2, C1 = -(-1+exp(2*j*a*k2)*(-k1**2+k2**2))/D, -4*exp(-j*a*k1+j*a*k2)*k1*k2/D
+    #B1, B2 = -2*k1*(k1+k2)/D, 2*exp(2*j*a*k2)*k1*(k1-k2)/D
+    
+    A2, C1 = (k1**2-k2**2)*sin(a*k2)/(2*j*k1*k2*cos(a*k2)+(k1**2+k2**2)*sin(a*k2)), 2*j*exp(-j*a*k1)*k1*k2/(2*j*k1*k2*cos(a*k2)+(k1**2+k2**2)*sin(k2*a))
+    B1, B2 = -2*k1*(k1+k2)/(exp(2*j*a*k2)*(k1-k2)**2-(k1+k2)**2), 2*exp(2*j*a*k2)*k1*(k1-k2)/(exp(2*j*a*k2)*(k1-k2)**2-(k1+k2)**2)
+    psai11 = A1*np.exp(j*k1*x)
+    psai12 = A2*np.exp(-j*k1*x)
+    psai21 = B1*np.exp(j*k2*x)
+    psai22 = B2*np.exp(-j*k2*x)
+    psai31 = C1*np.exp(j*k1*x)
+    psai32 = 0
+    # psai1 = A1*np.cos(k1*x)+A2*np.cos(-k1*x)
+    # psai2 = B1*np.exp(k2*x)+B2*np.exp(-k2*x)
+    # psai3 = C1*np.cos(k3*x)
+    if x in x1:
+        return psai11, psai12
+    elif x in x2:
+        return psai21, psai22
+    else:
+        return psai31, psai32
+    
+def polar(ratio,a,r,fai,V0,m):
+    rmax = a
+    r2 = np.linspace(0,rmax,100)
+    j=1j
+    k1, k2 = k0*np.sqrt(ratio)*np.sqrt(V0), k0*cm.sqrt(1-1/ratio)
+    A1, D1 = -(-k1*hankel2(m,a*k1)*h1vp(m,a*k1)+k1*hankel1(m,a*k1)*h2vp(m,a*k1))/(k2*hankel2(m,a*k1)*jvp(m,a*k2)-k1*jv(m,a*k2)*h2vp(m,a*k1)), -(k2*hankel1(m,a*k1)*jvp(m,a*k2)-k1*jv(m,a*k2)*h1vp(m,a*k1))/(k2*hankel2(m,a*k1)*jvp(m,a*k2)-k1*jv(m,a*k2)*h2vp(m,a*k1))
+    psai1 = (1*hankel1(m,k1*r)+D1*hankel2(m,k1*r))*exp(j*m*fai)
+    psai2 = A1*jv(m,k2*r)*exp(j*m*fai)
+    if r in r2:
+        return psai2
+    else:
+        return psai1
+    
+def cylindrical(ratio,a,h,r,fai,V0,m):
+    zmax = h 
+    rmax = a
+    z = np.linspace(-dc,zmax+dc,100)
+    z1 = np.linspace(-dc-zmax,0,100)
+    z2 = np.linspace(0,zmax,100)
+    z3 = np.linspace(zmax,zmax+dc,100)
+    r2 = np.linspace(0,rmax,100)
+    psair = polar(ratio,a,r,fai,V0,m)
+    psaiz1, psaiz2 = cartesian(ratio,a,h,V0)
+    
+    p1, p2 = psair*psaiz1, psair*psaiz2
+    
+    return p1, p2
+
+def Y(m,l,theta,fai):
+    j=1j
+    Y = lpmv(m,l,cos(theta))*exp(j*m*fai)
+    return Y
+
+def spherical(ratio,a,r,theta,fai,V0,l,m):
+    rmax = a
+    r2 = np.linspace(0,rmax,100)
+    j=1j
+    k1, k2 = k1, k2 = k0*np.sqrt(ratio)*np.sqrt(V0), k0*cm.sqrt(1-1/ratio)
+    C, D = -(-k2*hankel2(l,a*k1)*jvp(l,a*k2)+k1*jv(l,a*k2)*h2vp(l,a*k1))/(k1*(hankel2(l,a*k1)*h1vp(l,a*k1)-hankel1(l,a*k1)*h2vp(l,a*k1))), (k2*hankel1(l,a*k1)*jvp(l,a*k2)-k1*jv(l,a*k2)*h1vp(l,a*k1))/(k1*(hankel2(l,a*k1)*h1vp(l,a*k1)-hankel1(l,a*k1)*h2vp(l,a*k1)))
+    psai1 = (C*hankel1(l,k1*r)+D*hankel2(l,k1*r))*Y(m,l,theta,fai)
+    psai2 = 1*jv(l,k2*r)*Y(m,l,theta,fai)
+    if r in r2:
+        return psai2
+    else:
+        return psai1
+
+
+#plot
+def car_plot(V0,ratio,a):
+    V0, ratio, a =float(V0), float(ratio), float(a)
+    xmin, xmax = 0, a
+    x = np.linspace(-xmin-dc,xmax+dc, 300)
+    x1 = np.linspace(-dc,xmin,1000)
+    x2 = np.linspace(xmin,xmax,1000)
+    x3 = np.linspace(xmax,xmax+dc,1000)
+    y11, y12 = cartesian(ratio,a,x1,V0)
+    y21, y22 = cartesian(ratio,a,x2,V0)
+    y31, y32 = cartesian(ratio,a,x3,V0)
+    y1, y2 = [], []
+    for i in range(len(y11)):
+        y1.append(y11[i]+y12[i])
+        y2.append(y21[i]+y22[i])
+    yV0 = np.zeros(len(x))
+    yV0[(x>xmin) & (x<xmax)] = V0
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+
+    plt.rcParams['figure.figsize'] = (8.0,6.0)
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    ax1.plot(x1,y11,label='入射波')
+    ax1.plot(x1,y12,label='反射波')
+    ax1.plot(x1,y1,label='复合波1')
+    ax1.plot(x2,y2,label='复合波2')
+    ax1.plot(x3,y31,label='透射波')
+    ax1.plot(x,yV0,label='势')
+    ax1.set_xticks(np.arange(-xmin-dc,xmax+dc,0.2))
+    ax1.legend()
+    plt.title('一维情况下波函数幅值分布')
+    return fig
+
+def polar_plot(V0,ratio,a,m):
+    #V0, ratio, a, m =1,0.2,0.2,1
+    V0, ratio, a, m =float(V0), float(ratio), float(a), float(m)
+    rmin, rmax, faimax = 0, a, 2*pi
+
+    Z = np.zeros((400,400))
+    XX = np.linspace(-rmax-dc,rmax+dc,400)
+    x = list(XX)
+    YY = np.linspace(-rmax-dc,rmax+dc,400)
+    y = list(YY)
+    for i in range(len(x)):
+        if x[i] > 0:
+            for j in range(len(y)):
+                fai = arctan(y[j]/x[i])
+                r = sqrt(x[i]**2+y[j]**2)
+                Z[j][i] = polar(ratio,a,r,fai,V0,m).real
+        elif x[i]<0:
+            for j in range(len(y)):
+                fai = -arctan(y[j]/x[i])
+                r = sqrt(x[i]**2+y[j]**2)
+                Z[j][i] = polar(ratio,a,r,fai,V0,m).real 
+        else:
+            for j in range(len(y)):
+                if y[j] >= 0:
+                    fai = pi/2
+                else:
+                    fai = -pi/2
+                r = sqrt(x[i]**2+y[j]**2)
+                Z[j][i] = polar(ratio,a,r,fai,V0,m).real
+    X,Y = np.meshgrid(XX,YY)
+    # 设置中文字体
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+
+    # 创建图形和坐标轴
+    fig = plt.figure(figsize=(10, 5))  # 调整图形大小
+
+    # 使用 gridspec_kw 自定义子图布局
+    gs = fig.add_gridspec(1, 2, width_ratios=[2, 1.2])  # 调整比例
+
+    # 默认视角
+    ax1 = fig.add_subplot(gs[0], projection='3d')  # 占据两列
+    u = np.linspace(0, 2 * np.pi, 50)
+    h = np.linspace(0, V0, 20)
+    xp = np.outer(rmax * np.cos(u), np.ones(len(h)))
+    yp = np.outer(rmax * np.sin(u), np.ones(len(h)))
+    zp = np.outer(np.ones(len(u)), h)
+
+    # 使用更柔和的颜色
+    ax1.plot_surface(xp, yp, zp, color='black', alpha=0.2)  # 调整圆柱颜色和透明度
+    circle = Circle((0, 0), rmax, color='black', alpha=0.2)
+    ax1.add_patch(circle)
+    art3d.pathpatch_2d_to_3d(circle, z=V0, zdir="z")
+    ax1.plot_surface(X, Y, Z, cmap='coolwarm')  # 更改 colormap
+    ax1.view_init(elev=37, azim=41)  # 仰角 37 度，方位角 41 度
+    ax1.set_zlim(-1, 1)
+    ax1.set_xlabel('X')
+    ax1.set_ylabel('Y')
+    ax1.set_title("默认视角")
+
+    # 俯视
+    ax2 = fig.add_subplot(gs[1], projection='3d')  # 位于第二行第二列
+    ax2.plot_surface(xp, yp, zp, color='black', alpha=0.2)  # 调整圆柱颜色和透明度
+    circle = Circle((0,0), rmax, color='black', alpha=0.2)
+    ax2.add_patch(circle)
+    art3d.pathpatch_2d_to_3d(circle, z=V0, zdir="z")
+    ax2.plot_surface(X, Y, Z, cmap='coolwarm')  # 更改 colormap
+    ax2.view_init(elev=90, azim=0)  # 设置俯视视角
+    ax2.set_zlim(-1, 1)
+    ax2.set_xlabel('X')
+    ax2.set_ylabel('Y')
+    ax2.set_xticks([])  # 取消 x 轴刻度
+    ax2.set_yticks([])  # 取消 y 轴刻度
+    ax2.set_zticks([])  # 取消 z 轴刻度
+    ax2.set_title("俯视")
+
+    plt.tight_layout()  # 调整子图布局
+    plt.title('极坐标系下波函数幅值分布')
+    return fig
+
+def cylind_color(ratio,a,z,x,y,V0,m):
+    r = sqrt(x**2+y**2)
+    if x > 0:
+        fai = arctan(y/x)
+    elif x<0:
+        fai = -arctan(y/x)
+    else:
+        fai = pi/2
+    psai1, psai2 = cylindrical(ratio,a,z,r,fai,V0,m)
+    Normpsai = (abs(psai1+psai2))**2
+    return Normpsai
+
+def cylind_plot(V0,ratio,a,h,m):
+    #V0, ratio, a, h, m = 1, 0.2, 0.2, 1, 1
+    V0, ratio, a, h, m = float(V0), float(ratio), float(a), float(h), float(m)
+
+    rmax = a
+    zmax = h
+    XX = np.linspace(-rmax-dc,rmax+dc,20)
+    YY = np.linspace(-rmax-dc,rmax+dc,20)
+    ZZ = np.linspace(-zmax-dc,zmax+dc,20)
+    x,y,z = list(XX),list(YY),list(ZZ)
+
+    fig = plt.figure(figsize=(12, 8))  # 设置图形大小
+    ax = fig.add_subplot(111, projection='3d')
+
+    # 绘制散点图
+    values = []
+    absvalues = []
+    for i in x:
+        for j in y:
+            for k in z:
+                values.append(cylind_color(ratio, a, k, i, j, V0, m))
+                absvalues.append(abs(cylind_color(ratio,a,k,i,j,V0,m)))
+    vmid = (min(values)+max(values))*(ratio/7.5)
+    # 使用归一化对象对颜色映射进行调整
+    norm1 = Normalize(vmin=min(values), vmax=vmid)  # 设置第一个颜色条的范围
+    norm2 = Normalize(vmin=vmid, vmax=max(values))  # 设置第二个颜色条的范围
+    for i in x:
+        for j in y:
+            for k in z:
+                # 使用绝对值计算透明度
+                alpha = 0.5+0.5*abs(cylind_color(ratio, a, k, i, j, V0, m)) / max(absvalues)  
+                if cylind_color(ratio,a,k,i,j,V0,m)<=vmid:
+                    ax.scatter(i, j, k, c=cylind_color(ratio, a, k, i, j, V0, m), cmap='coolwarm', norm=norm1, alpha=alpha)
+                else:
+                    ax.scatter(i, j, k, c=cylind_color(ratio, a, k, i, j, V0, m), cmap='viridis', norm=norm2, alpha=alpha) 
+
+    # 绘制圆柱
+    u = np.linspace(0, 2 * np.pi, 50)
+    h = np.linspace(0, zmax, 20)
+    xp = np.outer(rmax * np.cos(u), np.ones(len(h)))
+    yp = np.outer(rmax * np.sin(u), np.ones(len(h)))
+    zp = np.outer(np.ones(len(u)), h)
+    ax.plot_surface(xp, yp, zp, color='black', alpha=0.2)
+
+    # 给圆柱加个‘盖子’
+    circle = Circle((0, 0), rmax, color='black', alpha=0.2)
+    ax.add_patch(circle)
+    art3d.pathpatch_2d_to_3d(circle, z=zmax, zdir="z")
+
+    # 设置坐标轴标签
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    # 添加颜色条
+    m1 = plt.cm.ScalarMappable(norm=norm1, cmap='coolwarm')
+    cb1 = plt.colorbar(m1, ax=ax, shrink=0.5, pad=0.1, location="left")  # 添加第一个颜色条
+    cb1.set_ticks([min(values), vmid])  # 设置刻度
+    cb1.set_ticklabels(['0', str(vmid)])  # 设置刻度标签
+    m2 = plt.cm.ScalarMappable(norm=norm2, cmap='viridis')
+    cb2 = plt.colorbar(m2, ax=ax, shrink=0.5, pad=0.1, location="right")  # 添加第二个颜色条
+    cb2.set_ticks([vmid, max(values)])  # 设置刻度
+    cb2.set_ticklabels([str(vmid), '1'])  # 设置刻度标签
+    plt.title('柱坐标系下波函数模方分布')
+
+    return fig
+
+def spher_color(ratio,a,z,x,y,V0,l,m):
+    r = sqrt(x**2+y**2+z**2)
+    if x> 0:
+        fai = arctan(y/x)
+    elif x<0:
+        fai = -arctan(y/x)
+    else:
+        fai = pi/2
+    if x==0 and y==0:
+        theta = pi/2
+    else:
+        theta = arctan(z/sqrt(x**2+y**2))
+    psai = spherical(ratio,a,r,theta,fai,V0,l,m)
+    normpsai = (abs(psai))**2
+    return normpsai
+
+def spher_plot(V0,ratio,a,m,l):
+    #V0, ratio, a, m, l =1,0.2,0.2,1,1
+    V0, ratio, a, m, l =float(V0), float(ratio), float(a), float(m), float(l)
+    rmax = a
+    XX = np.linspace(-rmax-dc,rmax+dc,15)
+    YY = np.linspace(-rmax-dc,rmax+dc,15)
+    ZZ = np.linspace(-rmax-dc,rmax+dc,15)
+    x,y,z = list(XX),list(YY),list(ZZ)
+
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111,projection='3d')
+
+    values = []
+    absvalues = []
+    for i in x:
+        for j in y:
+            for k in z:
+                values.append(spher_color(ratio,a,k,i,j,V0,l,m))
+                absvalues.append(abs(spher_color(ratio,a,k,i,j,V0,l,m)))
+
+    vmid = (min(values)+max(values))*(ratio/7.5)
+    # 创建第一个颜色条
+    norm1 = Normalize(vmin=min(values), vmax=vmid)  # 设置第一个颜色条的范围
+
+
+    # 创建第二个颜色条
+    norm2 = Normalize(vmin=vmid, vmax=max(values))  # 设置第二个颜色条的范围
+
+
+    for i in x:
+        for j in y:
+            for k in z:
+                alpha = 0.5+0.5*abs(spher_color(ratio,a,k,i,j,V0,l,m))/max(absvalues)
+                if spher_color(ratio,a,k,i,j,V0,l,m)<vmid:
+                    ax.scatter(i,j,k,c=spher_color(ratio,a,k,i,j,V0,l,m),cmap='coolwarm',norm=norm1,alpha=alpha)
+                else:
+                    ax.scatter(i,j,k,c=spher_color(ratio,a,k,i,j,V0,l,m),cmap='viridis',norm=norm2,alpha=alpha)
+    #绘制球体
+    u = np.linspace(0, 2 * np.pi, 50)
+    v = np.linspace(0, np.pi, 50)
+    x = rmax * np.outer(np.cos(u), np.sin(v))
+    y = rmax * np.outer(np.sin(u), np.sin(v))
+    z = rmax * np.outer(np.ones(np.size(u)), np.cos(v))
+    ax.plot_surface(x, y, z, color='black', alpha=0.1)
+
+    #设置坐标轴标签
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+
+    m1 = plt.cm.ScalarMappable(norm=norm1, cmap='coolwarm')  # 使用 coolwarm colormap
+    cb1 = plt.colorbar(m1, ax=ax, shrink=0.5, pad=0.1, location="left")  # 添加第一个颜色条
+    cb1.set_ticks([min(values),vmid])
+    cb1.set_ticklabels(['0',str(vmid/(max(values)+min(values)))])
+
+    m2 = plt.cm.ScalarMappable(norm=norm2, cmap='viridis')  # 使用 viridis colormap
+    cb2 = plt.colorbar(m2, ax=ax, shrink=0.5, pad=0.1, location="right")  # 添加第二个颜色条
+    cb2.set_ticks([vmid,max(values)])
+    cb2.set_ticklabels([str(vmid/(max(values)+min(values))),'1'])
+    plt.title('球坐标系下波函数模方分布')
+    return fig
+
+#animation
+def time_factor(V0,ratio,t):
+    j=1j
+    hbar=16/k0**2
+    E = exp(j*ratio*V0*t)
+    return E
+
+def ani_car_plot(V0,ratio,a):
+    #V0,ratio,a = 1,0.2 ,0.2
+    V0, ratio, a =float(V0), float(ratio), float(a)
+    xmin, xmax = 0, a
+    x = np.linspace(-xmin-dc,xmax+dc, 300)
+    x1 = np.linspace(-dc,xmin,1000)
+    x2 = np.linspace(xmin,xmax,1000)
+    x3 = np.linspace(xmax,xmax+dc,1000)
+
+
+
+    y11, y12 = cartesian(ratio,a,x1,V0)
+    y21, y22 = cartesian(ratio,a,x2,V0)
+    y31, y32 = cartesian(ratio,a,x3,V0)
+    y1, y2 = [], []
+    for i in range(len(y11)):
+        y1.append(y11[i]+y12[i])
+    for i in range(len(y21)):
+        y2.append(y21[i]+y22[i])
+    yV0 = np.zeros(len(x))
+    yV0[(x>xmin) & (x<xmax)] = V0
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,1,1)
+
+    plt.rcParams['figure.figsize'] = (8.0,6.0)
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    plt.rcParams['axes.unicode_minus'] = False
+    line11,=ax1.plot(x1,y11,label='入射波')
+    line12,=ax1.plot(x1,y12,label='反射波')
+    line1, =ax1.plot(x1,y1,label='复合波1')
+    line2 ,=ax1.plot(x2,y2,label='复合波2')
+    line31,=ax1.plot(x3,y31,label='透射波')
+    def init():
+        line11.set_ydata([np.nan]*len(x1))
+        line12.set_ydata([np.nan]*len(x1))
+        line1.set_ydata([np.nan]*len(x1))
+        line2.set_ydata([np.nan]*len(x2))
+        line31.set_ydata([np.nan]*len(x3))
+        return line1, ; line2, ; line11, ; line12, ; line31,
+
+    def update(frame):
+        for i in range(len(y11)):
+            y11[i] = (y11[i]*time_factor(V0,ratio,float(frame)/500))
+            y12[i] = (y12[i]*time_factor(V0,ratio,float(frame)/500))
+            y1[i] = (y1[i]*time_factor(V0,ratio,float(frame)/500))
+        for i in range(len(y2)):
+            y2[i] = (y2[i]*time_factor(V0,ratio,float(frame)/500))
+        for i in range(len(y31)):
+            y31[i] = (y31[i]*time_factor(V0,ratio,float(frame)/500))
+        line11.set_ydata(y11)
+        line12.set_ydata(y12)
+        line1.set_ydata(y1)
+        line2.set_ydata(y2)
+        line31.set_ydata(y31)
+        return line1,  line2,  line11,  line12,  line31,
+
+    ax1.plot(x,yV0,label='势')
+    ax1.set_xticks(np.arange(-xmin-dc,xmax+dc,0.2))
+    ax1.legend()
+    plt.title('一维情况下波函数幅值分布 E/V0={}'.format(ratio))
+    ani = FuncAnimation(fig,update,init_func=init,frames=2000,interval=50,blit=True)
+    return ani
---
+++ program.py
@@ -0,0 +1,256 @@
+'''
+项目目标：可视化“势垒贯穿和隧道效应”
+开发gui界面
+展示波函数势垒贯穿和隧道效应，二维、三维图
+gui界面的效果：
+坐标系——直角坐标、极坐标、三维柱坐标、球坐标
+四个按钮，每按一个会显示出对应的板块，可以更改势垒的边界从而改变波函数
+'''
+import tkinter as tk
+from tkinter import messagebox
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from plot import car_plot,polar_plot,cylind_plot,spher_plot,ani_car_plot
+import os
+from PIL import Image, ImageTk
+#Create main window
+root = tk.Tk()
+root.geometry('1200x500+900+50')
+root.title('势垒贯穿和隧道效应')
+
+#Defining of Functions
+#Button Widget
+
+
+
+def update():
+    global animation,ani
+    label5_in_frame1.grid_forget()
+    entry4_in_frame1.grid_forget()
+    label5_in_frame2.pack_forget()
+    label6_in_frame1.grid_forget()
+    entry5_in_frame1.grid_forget()
+    entry6_in_frame1.grid_forget()
+    label7_in_frame1.grid_forget()
+    labelwithbutton5_in_frame1.grid_forget()
+    button5_in_frame1.grid_forget()
+    animation = False
+    
+
+def chosebutton1():
+    update()
+    global coordinate
+    coordinate = 'cartesian'
+    label1_in_frame2.config(text=f"您选择的坐标系为：平面直角坐标系。")
+    label2_in_frame2.pack()
+    label3_in_frame2.pack()
+    label4_in_frame2.pack()
+    labelwithbutton5_in_frame1.grid(row=8,column=6)
+    button5_in_frame1.grid(row=9,column=6)
+
+def chosebutton2():
+    update()
+    global coordinate
+    coordinate = 'polar'
+    label1_in_frame2.config(text=f"您选择的坐标系为：平面极坐标系。")
+    label2_in_frame2.pack()
+    label3_in_frame2.pack()
+    label4_in_frame2.pack()
+    label5_in_frame1.config(text='请输入m(贝塞尔函数阶数)')
+    label5_in_frame1.grid(row=8,column=6)
+    entry4_in_frame1.grid(row=9,column=6,columnspan=3)
+    label5_in_frame2.pack()
+
+def chosebutton3():
+    update()
+    global coordinate
+    coordinate = 'cylindrical'
+    label1_in_frame2.config(text="您选择的坐标系为：柱坐标系。")
+    label2_in_frame2.pack()
+    label3_in_frame2.pack()
+    label4_in_frame2.pack()
+    label5_in_frame1.config(text='请输入m(贝塞尔函数阶数)')
+    label5_in_frame1.grid(row=8,column=6)
+    entry4_in_frame1.grid(row=9,column=6,columnspan=3)
+    label5_in_frame2.pack()
+    label6_in_frame1.grid(row=10,column=6)
+    entry5_in_frame1.grid(row=11,column=6,columnspan=3)
+    label6_in_frame2.pack()
+
+def chosebutton4():
+    update()
+    global coordinate
+    coordinate = 'spherical'
+    label1_in_frame2.config(text="您选择的坐标系为：球坐标系。")
+    label2_in_frame2.pack()
+    label3_in_frame2.pack()
+    label4_in_frame2.pack()
+    label5_in_frame1.config(text='请输入m（磁量子数）')
+    label5_in_frame1.grid(row=8,column=6)
+    entry4_in_frame1.grid(row=9,column=6,columnspan=3)
+    label5_in_frame2.pack()
+    label7_in_frame1.grid(row=10,column=6)
+    entry6_in_frame1.grid(row=11,column=6,columnspan=3)
+    label7_in_frame2.pack()
+
+def chosebutton5():
+    global animation
+    animation = True
+# def chosebutton6():
+#    label2_in_frame2.config(text='您选择的势垒厚度为：2a')
+# def chosebutton7():
+#     label2_in_frame2.config(text='您选择的势垒厚度为：3a')
+def generatebutton8():
+    a = entry1_in_frame1.get()
+    label2_in_frame2.config(text='您输入的势垒厚度为：{}'.format(a))
+    ratio = entry2_in_frame1.get()
+    label3_in_frame2.config(text='您输入的E/V0为：{}'.format(ratio))
+    V0 = entry3_in_frame1.get()
+    label4_in_frame2.config(text='您输入的V0为：{}'.format(V0))
+    m = entry4_in_frame1.get()
+    label5_in_frame2.config(text='您输入的m为：{}'.format (m))
+    h = entry5_in_frame1.get()
+    label6_in_frame2.config(text='您输入的h为：{}'.format(h))
+    l = entry6_in_frame1.get()
+    label7_in_frame2.config(text='您输入的l为：{}'.format(l))
+    global image
+     
+    if coordinate == 'cartesian':
+        fig = car_plot(V0,ratio,a)
+        if animation:
+            if not os.path.exists('image'):
+                os.makedirs('image')
+            if(os.path.isfile('image/gif.gif')):
+                os.remove('image/gif.gif')
+                print('Delete successfully')
+            animation_box(V0,ratio,a)
+    elif coordinate == 'polar':
+        fig = polar_plot(V0,ratio,a,m)
+    elif coordinate == 'cylindrical':
+        fig = cylind_plot(V0,ratio,a,h,m)
+    elif coordinate == 'spherical':
+        fig = spher_plot(V0,ratio,a,m,l)
+    else:
+        return messagebox1()
+    
+    image.get_tk_widget().destroy()
+    image = FigureCanvasTkAgg(fig, master = frame3)
+    image.draw()
+    image.get_tk_widget().pack()
+
+def animation_box(V0,ratio,a):
+        ani = ani_car_plot(V0,ratio,a)
+        ani.save('image/gif.gif',writer='pillow',fps=30)
+        img = Image.open('image/gif.gif')
+        def showgif(img):
+            top = tk.Toplevel()
+            top.title('一维势垒下波函数随时间变化')
+            num_frames = img.n_frames
+            frame_index = 0
+            def animate_gif():
+                nonlocal frame_index
+                img.seek(frame_index)
+                photo = ImageTk.PhotoImage(img)
+                giflabel.config(image=photo)
+                giflabel.image = photo
+                frame_index = (frame_index+1)%num_frames
+                top.after(50,animate_gif)
+            giflabel = tk.Label(top)
+            giflabel.pack()
+            close_button = tk.Button(top,text='关闭',command=top.destroy)
+            close_button.pack()
+            animate_gif()
+        showgif(img)
+
+
+#Dialog Box
+def messagebox1():
+    messagebox.showinfo('Note!','Warning! You have to choose a coordinate before generating!')
+
+#Create Frames
+frame1 = tk.Frame(root,relief='ridge',width=400)#frame1用于输入
+frame2 = tk.Frame(root,relief='ridge',width=400)#frame2用于显示label
+frame3 = tk.Frame(root,relief='ridge',width=400)#frame3用于显示图
+frame1.pack(side='left',fill='both')
+frame2.pack(side='left',fill='both')
+frame3.pack(side='left',fill='both')
+#Create widges inside the frames
+#frame1
+label1_in_frame1 = tk.Label(frame1, text='请选择坐标系')
+button1_in_frame1 = tk.Button(frame1,text='平面直角坐标',command=chosebutton1)
+button2_in_frame1 = tk.Button(frame1,text='平面极坐标',command=chosebutton2)
+button3_in_frame1 = tk.Button(frame1,text='柱坐标',command=chosebutton3)
+button4_in_frame1 = tk.Button(frame1,text='球坐标',command=chosebutton4)
+label2_in_frame1 = tk.Label(frame1,text='请输入势垒的厚度')
+entry1_in_frame1 = tk.Entry(frame1,text='a')
+button5_in_frame1 = tk.Checkbutton(frame1,text='动画',command=chosebutton5)
+labelwithbutton5_in_frame1 = tk.Label(frame1,text='可选择生成动画')
+# button6_in_frame1 = tk.Button(frame1,text='2a',command=chosebutton6)
+# button7_in_frame1 = tk.Button(frame1,text='3a',command=chosebutton7)
+button8_in_frame1 = tk.Button(frame1,text='生成',command=generatebutton8)
+label3_in_frame1 = tk.Label(frame1,text='请输入E/V0')
+entry2_in_frame1 = tk.Entry(frame1,text='E/V0')
+label4_in_frame1 = tk.Label(frame1,text='请输入V0')
+entry3_in_frame1 = tk.Entry(frame1,text='V0')
+label5_in_frame1 = tk.Label(frame1,text='请输入m(贝塞尔函数阶数)')
+entry4_in_frame1 = tk.Entry(frame1,text='m')
+label6_in_frame1 = tk.Label(frame1,text='请输入h（圆柱区域的高度）')
+entry5_in_frame1 = tk.Entry(frame1,text='h')
+label7_in_frame1 = tk.Label(frame1,text='请输入l（角动量量子数）')
+entry6_in_frame1 = tk.Entry(frame1,text='l')
+
+#frame1.grid
+label1_in_frame1.grid(row=0,column=6)
+button1_in_frame1.grid(row=1,column=0,columnspan=3)
+button2_in_frame1.grid(row=1,column=4,columnspan=3)
+button3_in_frame1.grid(row=1,column=8,columnspan=3)
+button4_in_frame1.grid(row=1,column=11,columnspan=3)
+label2_in_frame1.grid(row=2,column=6)
+entry1_in_frame1.grid(row=3,column=6,columnspan=3)
+# button5_in_frame1.grid(row=3,column=0,columnspan=3)
+# button6_in_frame1.grid(row=3,column=4,columnspan=3)
+# button7_in_frame1.grid(row=3,column=8,columnspan=3)
+label3_in_frame1.grid(row=4,column=6)
+entry2_in_frame1.grid(row=5,column=6,columnspan=3)
+label4_in_frame1.grid(row=6,column=6)
+entry3_in_frame1.grid(row=7,column=6,columnspan=3)
+button8_in_frame1.grid(row=15,column=6,columnspan=3)
+
+#frame2
+label1_in_frame2 = tk.Label(frame2, text='等待选择')
+label2_in_frame2 = tk.Label(frame2,text='请输入势垒的厚度a')
+label3_in_frame2 = tk.Label(frame2,text='请输入E/V0')
+label4_in_frame2 = tk.Label(frame2,text='请输入V0')
+label5_in_frame2 = tk.Label(frame2,text='请输入m')
+label6_in_frame2 = tk.Label(frame2,text='请输入h')
+label7_in_frame2 = tk.Label(frame2,text='请输入l')
+
+#frame2.grid/pack
+label1_in_frame2.pack()
+
+#frame3
+label1_in_frame3 = tk.Label(frame3, text='绘图展示')
+label1_in_frame3.pack()
+
+
+
+
+
+#initialize
+coordinate = 0
+entry1_in_frame1.insert(0,'0.2')#a
+entry2_in_frame1.insert(0,'0.2')#ratio
+entry3_in_frame1.insert(0,'1')#V0
+entry4_in_frame1.insert(0,'1')#m
+entry5_in_frame1.insert(0,'1')#h
+entry6_in_frame1.insert(0,'1')#l
+
+fig = plt.figure()
+fig.patch.set_facecolor('white')
+image = FigureCanvasTkAgg(fig, master = frame3)
+image.draw()
+image.get_tk_widget().pack()
+animation = False
+
+
+root.mainloop()
